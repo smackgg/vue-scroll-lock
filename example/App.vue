48c56f5e07f8a2bd62cb85d3d06c54018fcfcef9
smackgg <smackgg@gmail.com>
2017-10-11 14:16:39 +0800

    demo

--- example/App.vue
+++ example/App.vue
@@ -2,25 +2,39 @@
   <div id="app">
     <tactful-scroll class="wrapper">
       <div class="content">
+        <h2>Content</h2>
       </div>
     </tactful-scroll>
   </div>
 </template>
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  html {
+    height: 100%;
+  }
   body {
     height: 2000px;
-    background: #ccc;
+    background: #2196f3;
+    padding: 30px;
+    background-image: url('./assets/icon_2.png');
+    background-size: 200px 200px;
   }
   .wrapper {
-    width: 100%;
-    height: 400px;
+    width: 70%;
+    height: 70vh;
+    box-sizing: border-box;
     background: #000;
     overflow: scroll;
+    padding: 20px;
+    line-height: 1.8;
   }
+
   .content {
-    width: 60%;
-    height: 1000px;
-    background: red;
+    height: 2000px;
+    background-image: url('./assets/icon_1.png');
   }
 
 </style>
