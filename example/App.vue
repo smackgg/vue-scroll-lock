e4ca122b4ba0f37230e8af0a0b3ac28f59f0ab90
smackgg <smackgg@gmail.com>
2018-01-12 16:12:14 +0800

    fork github button

--- example/App.vue
+++ example/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <a href="https://github.com/smackgg/vue-scroll-lock"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
     <div class="wrapper markdown-body" :class="{
         'modal--show': showModal
       }">
@@ -125,7 +126,11 @@ export default {
   }
 
   body {
-    padding: 20px;
+    box-sizing: border-box;
+    min-width: 200px;
+    max-width: 980px;
+    margin: 0 auto;
+    padding: 45px;
   }
 
   .wrapper.modal--show {
